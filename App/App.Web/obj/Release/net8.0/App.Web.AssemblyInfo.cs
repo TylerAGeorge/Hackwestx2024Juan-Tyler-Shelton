111785c4e1195a0b618663052d9d37d142910d95
TylerAGeorge <tylergeorgepersonal@gmail.com>
2024-09-14 11:23:16 -0500

    Created placeholder for FAQs

--- App/App.Web/obj/Release/net8.0/App.Web.AssemblyInfo.cs
+++ App/App.Web/obj/Release/net8.0/App.Web.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("App.Web")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbae90c83621c4a1faa3df0fccb93752a6c7ac27")]
 [assembly: System.Reflection.AssemblyProductAttribute("App.Web")]
