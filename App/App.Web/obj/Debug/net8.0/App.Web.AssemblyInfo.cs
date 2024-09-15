ce58bfccd376f10488a46dd9875aacda7dad0ecb
TylerAGeorge <tylergeorgepersonal@gmail.com>
2024-09-14 20:40:03 -0500

    FAQ api access working

--- App/App.Web/obj/Debug/net8.0/App.Web.AssemblyInfo.cs
+++ App/App.Web/obj/Debug/net8.0/App.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9414d6ad8a96c75c359db420dfc732dcdf49a1d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e3eb886bc0ef86ad9cd1c5c25fd4b8f06ac2997")]
 [assembly: System.Reflection.AssemblyProductAttribute("App.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
